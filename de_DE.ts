ac3a7350525b9681e8edcaa730b2586beb9a6020
Tobirium <tobirium@gmail.com>
2018-12-04 19:11:39 +0000

    Translated using Weblate (German)
    Currently translated at 99.4% (1086 of 1093 strings)
    
    Translation: Hiri/Translations
    Translate-URL: https://hosted.weblate.org/projects/hiri/translations/de/

--- de_DE.ts
+++ de_DE.ts
@@ -4627,7 +4627,7 @@ Inbox Zero was developed by productivity expert Merlin Mann. According to Mann, 
     </message>
     <message>
         <source>Have any questions about your trial or feedback that you&apos;d like to share?</source>
-        <translation type="unfinished"></translation>
+        <translation>Haben Sie Fragen zu Ihrer Testphase oder möchten ein Feedback mit uns teilen?</translation>
     </message>
     <message>
         <source>Send</source>
